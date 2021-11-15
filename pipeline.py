e9f5fd891278e4e081aa6538bc17a5a74ea49b63
Aukkawut Ammartayakun <aukkawut@MacBook-Air.local>
2021-11-15 08:19:24 -0500

    update plot function, WIP

--- pipeline.py
+++ pipeline.py
@@ -11,8 +11,16 @@ def loaddata(filename):
     data = pd.read_csv(filename)
     return data
 
-def plot_data(data,row):
+def plot_data(data,row,end=False):
     '''
-    This function will plot the data at row row from dataset 
+    This function will plot the data at row row from dataset, WIP
     '''
-    
+    #convert the data to numpy array with size 25,25.
+    if end:
+        d = data.iloc[row,1:625].to_numpy().reshape(25,25)
+    else:
+        d = data.iloc[row,626:1251].to_numpy().reshape(25,25)
+    #plot the data
+    plt.imshow(d,cmap='gray',interpolation='nearest')
+    plt.show()
+
